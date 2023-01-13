0dd589435aeb54cd1a65c50f059b811d91cb3098
ffclemente <ffcgpp@gmail.com>
2023-01-12 22:44:26 -0300

    analise espacial

--- 1. Script - Mapa Brasil IDH. Script - Mapa Brasil IDH.r
+++ 1. Script - Mapa Brasil IDH. Script - Mapa Brasil IDH.r
@@ -1,3 +1,4 @@
+#CRIANDO UM MAPA DO BRASIL COM OS IDHS DOS ESTADOS
 # Instalação de pacotes necessários
 
 pacotes <- c("rgdal","raster","tmap","maptools","sf","rgeos","sp","adehabitatHR",
@@ -15,46 +16,46 @@ if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
   sapply(pacotes, require, character = T) 
 }
 
+#excluindo pacotes do environment
+rm(pacotes)
+
 # Baixar MAPA do BRASIL no site do IBGE: https://www.ibge.gov.br/geociencias/organizacao-do-territorio/malhas-territoriais/15774-malhas.html?=&t=acesso-ao-produto
 # IBGE > Malha Municipal > Brasil > Unidades da Federação.
 # o arquivo é um zip com 5 itens intitulado "BR_UF_2021".
 # o formato do arquivo é SHAPEFILE.
 
 # Ler arquivos "BR_UF_2021" na pasta a qual intitulei de "shapefile_brasil".
-# O nome do objeto criado será "shp_br" de shapefile Brasil.
 
-shp_br <- readOGR(dsn = "shapefile_brasil", layer = "BR_UF_2021")
+mapa_br <- readOGR(dsn = "shapefile_brasil", layer = "BR_UF_2021")
 
 
 #Informações acerca do Mapa 
-summary(shp_br)
-class(shp_br)
-typeof(shp_br)
+summary(mapa_br)
+class(mapa_br)
+typeof(mapa_br)
 
 
 # Visualizando informações do Mapa
-shp_br@data %>% 
+mapa_br@data %>% 
   kable() %>%
   kable_styling(bootstrap_options = "striped", 
                 full_width = TRUE, 
                 font_size = 12)
 
-shp_br$NM_UF
-shp_br$SIGLA
-shp_br$NM_REGIAO
+mapa_br$NM_UF
+mapa_br$SIGLA
+mapa_br$NM_REGIAO
 
 # Para acessar os outros componentes do shapefile, utilizaremos o operador @:
 
-shp_br@polygons #Posições geográficas dos polígonos
-shp_br@plotOrder #Ordem de plotagem dos polígonos
-shp_br@bbox #Eixo X (Longitude Oeste e Leste; Latitude Norte e Sul)
-shp_br@proj4string@projargs #Sistema de projeção geográfica do shapefile
+mapa_br@polygons #Posições geográficas dos polígonos
+mapa_br@plotOrder #Ordem de plotagem dos polígonos
+mapa_br@bbox #Eixo X (Longitude Oeste e Leste; Latitude Norte e Sul)
+mapa_br@proj4string@projargs #Sistema de projeção geográfica do shapefile
 
 
 # PLOTAGEM do SHAPEFILE
-plot(shp_br)
-
-area(shp_br)
+plot(mapa_br)
 
 
 # BAIXAR PLANILHA COM IDHs: http://www.atlasbrasil.org.br/ranking
@@ -93,13 +94,13 @@ data_IDH_EstadosBR %>%
 # Para combinar os dados do objeto dados_sp com a base de dados de nosso 
 # shapefile, podemos utilizar a função merge():
 
-shp_dados_br <- merge(x = shp_br,
+mapa_br_merge <- merge(x = shp_br,
                       y = data_IDH_EstadosBR,
                       by.x = "CD_UF",
                       by.y = "Código_UF")
 
 
-shp_dados_br@data %>% 
+mapa_br_merge@data %>% 
   kable() %>%
   kable_styling(bootstrap_options = "striped", 
                 full_width = TRUE, 
@@ -109,15 +110,15 @@ shp_dados_br@data %>%
 
 #sALVANDO
 
-writeOGR(obj = shp_dados_sp, 
-         layer = "nosso_novo_shapefile", 
+writeOGR(obj = mapa_br_merge, 
+         layer = "novo_shapefile_brasil", 
          driver = "ESRI Shapefile", 
          dsn = "mbadsa")
 
 
 
 #PLOTAGEM HISTOGRAMA
-shp_dados_br@data %>% 
+mapa_br_merge@data %>% 
   ggplot() +
   geom_histogram(aes(x = IDHM),
                  fill = "deepskyblue4",
@@ -131,14 +132,14 @@ shp_dados_br@data %>%
 # Passo 1: Transformar o shapefile num objeto do tipo data frame e, depois,
 # importar os dados que já estavam no shapefile para o novo objeto data frame.
 
-shp_dados_br <- tidy(shp_dados_br, region = "CD_UF") %>% 
+mapa_br_merge <- tidy(shp_dados_br, region = "CD_UF") %>% 
   rename(CD_UF = id) %>% 
   left_join(shp_dados_br@data,
             by = "CD_UF")
 
 
 #Passo 2: PLOTAGEM GGPLOT
-shp_dados_br %>% 
+mapa_br_merge %>% 
   ggplot() +
   geom_polygon(aes(x = long, y = lat, group = group, fill = IDHM),
                color = "black") +
@@ -152,12 +153,16 @@ shp_dados_br %>%
 #Passo 3: PLOTAGEM GGPLOT INTERATIVA
 
 plotly::ggplotly ()
-shp_dados_br %>% 
+mapa_br_merge %>% 
   ggplot() +
   geom_polygon(aes(x = long, y = lat, group = group, fill = IDHM, label = NM_UF),
                color = "black") +
   labs(x = "Longitude",
        y = "Latitude",
-       fill = "IDHM") +
+       fill = "IDHM", "NM_UF") +
   scale_fill_viridis_c()+
   theme_bw()
+
+
+
+plot.new()
